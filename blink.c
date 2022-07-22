5a77d687dccd394f71432db556d06901a8b6df30
Vitor Bortoloti <vitorbortoloti@usp.br>
2022-07-22 15:10:58 -0300

    Blink

---
+++ blink.c
@@ -0,0 +1,61 @@
+#include <gpiod.h>
+#include <stdio.h>
+#include <unistd.h>
+
+#ifndef	CONSUMER
+#define	CONSUMER	"Consumer"
+#endif
+
+int main(int argc, char **argv)
+{
+	char *chipname = "gpiochip0";
+	unsigned int line_num = 64;	// GPIO Pin #64 -> C0
+	struct gpiod_chip *chip;
+	struct gpiod_line *line;
+	
+	int i, ret;	
+	unsigned int val;
+
+	//Inicialização do pino
+
+	chip = gpiod_chip_open_by_name(chipname);
+	if (!chip) {
+		perror("Open chip failed\n");
+		goto end;
+	}
+
+	line = gpiod_chip_get_line(chip, line_num);
+	if (!line) {
+		perror("Get line failed\n");
+		goto close_chip;
+	}
+
+	ret = gpiod_line_request_output(line, CONSUMER, 0); //setando como output
+	if (ret < 0) {
+		perror("Request line as output failed\n");
+		goto release_line;
+	}
+	//Init end
+	
+
+	/* Blink 20 times */
+	val = 0;
+	for (i = 20; i > 0; i--) {
+		//
+		ret = gpiod_line_set_value(line, val);		//escrevendo valor
+		if (ret < 0) {
+			perror("Set line output failed\n");	
+			goto release_line;
+		}
+		printf("Output %u on line #%u\n", val, line_num);
+		sleep(1);
+		val = !val;
+	}
+
+release_line:
+	gpiod_line_release(line);
+close_chip:
+	gpiod_chip_close(chip);
+end:
+	return 0;
+}
