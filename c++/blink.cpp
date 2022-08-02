fb650b19b20e56c5f53d49b10c1e2cb8d23ee889
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-08-02 15:47:22 -0300

    fix

--- c++/blink.cpp
+++ c++/blink.cpp
@@ -1,35 +1,59 @@
-#include <gpiod.h>
-#include <stdio.h>
-#include <unistd.h>
+/**
+ * Program for creating a simple 50% duty cycle pwm signal on a pin.
+ * Change the `OUTPUT_PIN` define to change the output pin.
+ * Add the desired frequency as a command line argument.
+ *
+ * Close the program by a simple CTRL + C.
+ */
+#include <iostream>
+#include <thread>
+#include <chrono>
+#include <csignal>
+#include <gpiod.hpp>
 
-int main(int argc, char **argv)
-{
-  const char *chipname = "gpiochip0";
-  struct gpiod_chip *chip;
-  struct gpiod_line *led;    // Red LED
+#define OUTPUT_PIN 65
+static bool run = true;
 
-  int i, val;
+int main(int argc, char* argv[]) {
 
-  // Open GPIO chip
-  chip = gpiod_chip_open_by_name(chipname);
+    
+    long frequency = 1;
+    
+    auto chip = gpiod::chip("0", gpiod::chip::OPEN_BY_NUMBER);
+    if (!chip) {
+        std::cerr << "Could not find/ open gpio chip" << std::endl;
+        return 2;
+    }
+    auto line = chip.get_line(OUTPUT_PIN);
+    if (!line) {
+        std::cerr << "Could not find output line" << std::endl;
+        return 3;
+    }
+    if (line.is_used()) {
+        std::cerr << "output line is already used!" << std::endl;
+        return 4;
+    }
 
-  // Open GPIO lines
-  led = gpiod_chip_get_line(chip, 24);
+    gpiod::line_request request;
+    request.request_type = gpiod::line_request::DIRECTION_OUTPUT;
+    request.consumer = "User Led";
 
-  // Open LED  for output
-  gpiod_line_request_output(led, "example1", 0);
+    line.request(request);
 
-  // Blink LEDs in a binary pattern
-  i = 0;
-  while (true) {
-  
-    usleep(100000);
-    i++;
-  }
+    bool state = false;
 
-  // Release lines and chip
-  gpiod_line_release(led);
+    auto duration = std::chrono::microseconds((S_IN_US / frequency));
+    
+    while (1) {
+        auto start = std::chrono::system_clock::now();
+        line.set_value(state);
+        state = !state;
 
-  gpiod_chip_close(chip);
-  return 0;
+        std::this_thread::sleep_until(start + duration);
+    }
+    
+    line.set_value(0);
+    line.release();
+
+    return 0;
 }
