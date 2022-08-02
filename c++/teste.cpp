9ce456b28272389835c493e5f1dcf3958d151c56
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-08-02 16:28:38 -0300

    att

--- c++/teste.cpp
+++ c++/teste.cpp
@@ -1,42 +1,24 @@
-#include <gpiod.hpp>
+// Use gpio drivers to toggle a single GPIO
+// line on Raspberry Pi
 
-#include <cstdlib>
+// Use following commands to install prerequisites and build
+// sudo apt install gpiod
+// sudo apt install libgpiod-dev
+// g++ -Wall -o gpio gpip.cpp -lgpiodcxx
+
 #include <iostream>
-
-int main(int argc, char **argv)
-{
-	if (argc != 1) {
-		::std::cerr << "usage: " << argv[0] << ::std::endl;
-		return EXIT_FAILURE;
-	}
-
-	for (auto& cit: ::gpiod::make_chip_iter()) {
-		::std::cout << cit.name() << " - " << cit.num_lines() << " lines:" << ::std::endl;
-
-		for (auto& lit: ::gpiod::line_iter(cit)) {
-			::std::cout << "\tline ";
-			::std::cout.width(3);
-			::std::cout << lit.offset() << ": ";
-
-			::std::cout.width(12);
-			::std::cout << (lit.name().empty() ? "unnamed" : lit.name());
-			::std::cout << " ";
-
-			::std::cout.width(12);
-			::std::cout << (lit.consumer().empty() ? "unused" : lit.consumer());
-			::std::cout << " ";
-
-			::std::cout.width(8);
-			::std::cout << (lit.direction() == ::gpiod::line::DIRECTION_INPUT ? "input" : "output");
-			::std::cout << " ";
-
-			::std::cout.width(10);
-			::std::cout << (lit.active_state() == ::gpiod::line::ACTIVE_LOW
-								? "active-low" : "active-high");
-
-			::std::cout << ::std::endl;
-		}
-	}
-
-	return EXIT_SUCCESS;
+#include <gpiod.hpp>
+#include <unistd.h>
+ 
+int main(void)
+{ 
+   ::gpiod::chip chip("gpiochip0");
+   
+   auto line = chip.get_line(17);  // GPIO17
+   line.request({"example", gpiod::line_request::DIRECTION_OUTPUT, 0},1);  
+   
+   sleep(0.1);
+   
+   line.set_value(0);
+   line.release();
 }
