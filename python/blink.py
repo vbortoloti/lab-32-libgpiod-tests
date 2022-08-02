a5b8ab8ad756ce9c6f1af82ed2e765912cee8f56
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-08-02 17:05:52 -0300

    Update blink.py

--- python/blink.py
+++ python/blink.py
@@ -2,12 +2,23 @@
 
 
 import gpiod
-import sys
 
 if __name__ == '__main__':
-    line = gpiod.find_line(sys.argv[0])
-    if line is None:
-        sys.exit(1)
+    for chip in gpiod.ChipIter():
+        print('{} - {} lines:'.format(chip.name(), chip.num_lines()))
 
-    print('{} {}'.format(line.owner().name(), line.offset()))
-    line.owner().close()    
+        for line in gpiod.LineIter(chip):
+            offset = line.offset()
+            name = line.name()
+            consumer = line.consumer()
+            direction = line.direction()
+            active_state = line.active_state()
+
+            print('\tline {:>3}: {:>18} {:>12} {:>8} {:>10}'.format(
+                    offset,
+                    'unnamed' if name is None else name,
+                    'unused' if consumer is None else consumer,
+                    'input' if direction == gpiod.Line.DIRECTION_INPUT else 'output',
+                    'active-low' if active_state == gpiod.Line.ACTIVE_LOW else 'active-high'))
+
+        chip.close()
