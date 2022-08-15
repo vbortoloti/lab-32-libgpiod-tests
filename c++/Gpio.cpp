1388dff5cfc247683d5daef85f1ee96536fed601
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-08-15 16:28:10 -0300

    att

--- c++/Gpio.cpp
+++ c++/Gpio.cpp
@@ -4,13 +4,17 @@
 #include <string>
 #include <iostream>
 
-Gpio::Gpio(Pin pin, int mode, std::string alias)
+Gpio::Gpio(std::string pin, int mode, std::string alias)
 {
-    this->pin = pin;
-    this->mode = mode;
-    this->alias = alias;
-    chipNumber = "0";
-    enable();
+    this->pin = getPin(pin);
+    if(this->pin!=-1){
+        this->mode = mode;
+        this->alias = alias;
+        chipNumber = "0";
+        enable();
+    }else{
+        error(1);
+    }
 }
 Gpio::~Gpio(){
     releaseAll();
@@ -77,6 +81,9 @@ int Gpio::read()
 void Gpio::error(int num){
     switch (num)
     {
+    case 1:
+        std::cerr << "Invalid GPIO" << std::endl;
+        break;
     case 2:
         std::cerr << "Could not find/ open gpio chip" << std::endl;
         break;
@@ -91,3 +98,76 @@ void Gpio::error(int num){
     }
 }
 
+int getPin(std::string pinName){
+    switch (pinName)
+    {
+    case "A28":
+        return 28;
+        break;
+    case "B0":
+        return 32;
+        break;
+    case "B1":
+        return 33;
+        break;
+    case "B2":
+        return 34;
+        break;
+    case "B8":
+        return 40;
+        break;
+    case "B10":
+        return 42;
+        break;
+    case "B13":
+        return 45;
+        break;
+    case "B15":
+        return 47;
+        break;
+    case "B16":
+        return 48;
+        break;
+    case "B18":
+        return 50;
+        break;
+    case "B19":
+        return 51;
+        break;
+    case "C0":
+        return 64;
+        break;
+    case "C1":
+        return 65;
+        break;
+    case "C4":
+        return 67;
+        break;
+    case "C5":
+        return 69;
+        break;
+    case "C6":
+        return 70;
+        break;
+    case "C23":
+        return 87;
+        break;
+    case "C24":
+        return 88;
+        break;
+    case "D30":
+        return 126;
+        break;
+    case "E2":
+        return 130;
+        break;
+    case "E3":
+        return 131;
+        break;
+        
+        
+    default:
+        return -1;
+        break;
+    }
+}
--- c++/Gpio.h
+++ c++/Gpio.h
@@ -6,49 +6,26 @@
 class Gpio
 {
 private:
-    enum Pin
-    {   
-        A28 = 28,
-        B0  = 32,
-        B1  = 33,
-        B2  = 34,
-        B8  = 40,
-        B10 = 42,
-        B13 = 45,
-        B15 = 47,
-        B16 = 48,
-        B18 = 50,
-        B19 = 51, 
-        C0  = 64, 
-        C1  = 65, 
-        C4  = 68,
-        C5  = 69,
-        C6  = 70,
-        C23 = 87,
-        C24 = 88,
-        D30 = 126,
-        E2  = 130,
-        E3  = 131  
-    };
-
     std::string alias;
     ::gpiod::chip chip;
     std::string chipNumber;
     ::gpiod::line line;
-    Pin pin;
+
+    int pin;
     int mode; // Output; input; interrupt
     int state;
 
-public:
-    Gpio(Pin pin, int mode, std::string alias);
-    ~Gpio();
     void enable();
     void releaseAll();
+    void error(int num);
+    int getPin(std::string pinName);
+public:
+    Gpio(std::string pin, int mode, std::string alias);
+    ~Gpio();
 
     void write(int state);
     int read();
     void attachInterrupt();
     
-    void error(int num);
 };
 #endif
