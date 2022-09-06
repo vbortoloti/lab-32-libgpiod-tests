14498a2f916a5ef6021bd3ce91f45fe29e8b4b96
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-09-06 16:57:17 -0300

    add read example

---
+++ python/read.py
@@ -0,0 +1,11 @@
+import gpiod
+import sys
+
+chip=gpiod.Chip('gpiochip0')
+lines = chip.get_lines([65])
+lines.request(consumer="teste", type=gpiod.LINE_REQ_DIR_IN)
+vals = lines.get_values()
+
+for val in vals:
+    print(val, end=' ')
+print()
