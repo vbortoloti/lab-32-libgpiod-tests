26039c7f49186e7093ec4dbb0eaf214d67097b63
Vitor Bortoloti <bortoloti.vitor@gmail.com>
2022-09-06 17:25:03 -0300

    fix

--- python/read.py
+++ python/read.py
@@ -1,7 +1,7 @@
 import gpiod
 import sys
 
-chip=gpiod.Chip('gpiochip0')
+chip=gpiod.chip("/dev/gpiochip0")
 lines = chip.get_lines([65])
 lines.request(consumer="teste", type=gpiod.LINE_REQ_DIR_IN)
 vals = lines.get_values()
